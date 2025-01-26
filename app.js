7c66b38956d10e4c7acc1aaa04b7627962c2bed6
Ricardo Henriquez <101915304+ricardoHP0@users.noreply.github.com>
2025-01-26 11:08:44 -0400

    Se Implemento una función para agregar amigos a array

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+let inputAmigo = document.getElementById('amigo');
+
+function agregarAmigo() {
+    if(inputAmigo.value !== '') {
+        amigos.push(inputAmigo.value);
+        console.log(amigos);
+        inputAmigo.value = '';
+    }
+    else {
+        alert('Por favor, inserte un nombre.');
+    }
+}
+
